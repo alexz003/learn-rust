4806fd358ac0a267598db835f9051bd29c85fd9e
alexz003 <johnalexmarich@gmail.com>
2026-05-12 20:41:31 -0400

    reviewed control flow options

--- projects/sandbox/src/main.rs
+++ projects/sandbox/src/main.rs
@@ -4,7 +4,8 @@ fn main() {
     // variables_mutability_shadowing();
     // overflow();
     // data_types();
-    parameter(5);
+    // parameter(10);
+    // control_flow();
 }
 
 fn variables_mutability_shadowing() {
@@ -100,4 +101,70 @@ fn imm_param(y: i32) -> i32 {
 
 fn tup_return(y: u8) -> (u8, u8) {
     (5, y)
+}
+
+fn control_flow() {
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of the number is {number}");
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2
+        }
+    };
+
+    println!("Result {result}");
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    let mut number = 3;
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        // println!("the value is: {a[index]}"); // Not Valid
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    for number in (1..=4).rev() {
+        println!("number: {number}");
+    }
 }
